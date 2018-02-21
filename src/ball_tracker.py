d96c894f545f5897b00c02cf14dde281d2ed4ddb
Drew Warren <drewbwarren@gmail.com>
2018-02-21 14:56:53 -0600

    Ball tracker started
    A simple color filter based on the hackathon challenge. The ball_tracker
    node takes the image topic and separates the orange ball from the rest
    of the picture and returns the pixel coordinates center of the ball.

--- src/ball_tracker.py
+++ src/ball_tracker.py
@@ -1,4 +1,7 @@
-import rospy from sensor_msgs.msg import Image
+#!/usr/bin/env python
+
+import rospy 
+from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
@@ -8,6 +11,10 @@ class ball_tracking:
     def __init__(self):
         self.image_sub = rospy.Subscriber('usb_cam/image_raw', Image, self.image_callback)
         self.bridge = CvBridge()
+        self.filterx = [0,0,0,0,0,0,0,0,0,0]
+        self.filtery = [0,0,0,0,0,0,0,0,0,0]
+        self.x = 0
+        self.y = 0
 
     def image_callback(self, data):
         try:
@@ -21,14 +28,14 @@ class ball_tracking:
         ######
 
         # Color filter for orange ping pong ball
-        lower = np.array([0,100,100])
-        upper = np.array([9,255,255])
+        lower = np.array([10,150,150])
+        upper = np.array([65,255,255])
 
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower, upper)
         res = cv2.bitwise_and(cv_image,cv_image, mask=mask) 
 
-        erod = cv2.erode(mask, None, iterations=2)
+        erode = cv2.erode(mask, None, iterations=2)
         dilate = cv2.dilate(erode, None, iterations=2)
         _, contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -46,24 +53,36 @@ class ball_tracking:
 
         # some filtering
 
-        self.filterx[1:10] = self.filterx[0:9]
-        self.filterx[0] = x_max
-        if np.abs(x_max - self.x) > 10 :
-            self.x = np.mean(self.filterx[0:5])
-        else:
-            self.x = np.mean(self.filterx[0:8])
+#        self.filterx[1:10] = self.filterx[0:9]
+#        self.filterx[0] = x_max
+#        if np.abs(x_max - self.x) > 10 :
+#            self.x = np.mean(self.filterx[0:5])
+#        else:
+#            self.x = np.mean(self.filterx[0:8])
+#
+#        self.filtery[1:10] = self.filtery[0:9]
+#        self.filtery[0] = y_max
+#        if np.abs(y_max - self.y) > 10:
+#           self.y = np.mean(self.filtery[0:5])
+#        else:
+#            self.y = np.mean(self.filtery[0:8])
 
-        self.filtery[1:10] = self.filtery[0:9]
-        self.filtery[0] = y_max
-        if np.abs(y_max - self.y) > 10:
-           self.y = np.mean(self.filtery[0:5])
-        else:
-            self.y = np.mean(self.filtery[0:8])
-
+        self.x = x_max
+        self.y = y_max
 
         cv2.circle(cv_image, (int(self.x), int(self.y)), 6, [255,255,255], -1)
 
         cv2.imshow("Image window", cv_image)
+        cv2.imshow("color separate",res)
         cv2.waitKey(1)
 
+if __name__ == '__main__':
 
+    rospy.init_node('my_img_processor', anonymous=True)
+
+    bt = ball_tracking()
+    
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("shutting Down")
