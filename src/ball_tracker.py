c15d8ebc0b8ed424395925d187c43f8d79aad167
Drew Warren <drewbwarren@gmail.com>
2018-02-24 16:15:59 -0600

    ball_tracker can locate the corners of the platform and transform the image

--- src/ball_tracker.py
+++ src/ball_tracker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import rospy 
+import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
@@ -16,13 +16,27 @@ class ball_tracking:
         self.x = 0
         self.y = 0
 
+        self.corners = [[0,0],[0,0],[0,0],[0,0]]
+        self.corner_filter = [[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]], \
+            [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]], \
+            [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]], \
+            [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]]
+
+        self.start_time = rospy.Time.now()
+
     def image_callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
         except CvBridgeError, e:
             print("---Camera Manage---", e)
 
-        
+
+        platform_img = self.perspective_image(cv_image)
+
+
+
+
+    def ball_locator(self, thing):
         ######
         # Blob Tracker, color based
         ######
@@ -35,9 +49,9 @@ class ball_tracking:
         #lower = np.array([0,0,0])
         #upper = np.array([255,255,65])
 
-        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(platform_img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower, upper)
-        res = cv2.bitwise_and(cv_image,cv_image, mask=mask) 
+        res = cv2.bitwise_and(cv_image,cv_image, mask=mask)
 
         #erode = cv2.erode(mask, None, iterations=2)
         #dilate = cv2.dilate(erode, None, iterations=2)
@@ -76,16 +90,79 @@ class ball_tracking:
 
         cv2.circle(cv_image, (int(self.x), int(self.y)), 6, [255,255,255], -1)
 
-        cv2.imshow("Image window", cv_image)
-        cv2.imshow("color separate",res)
-        cv2.waitKey(1)
+        #cv2.imshow("Image window", cv_image)
+        #cv2.imshow("color separate",res)
+        #cv2.waitKey(1)
+
+
+
+    def perspective_image(self, img):
+
+        # Color filter for the black platform
+        lower = np.array([0,int(0*255/100.0),int(5*255/100.0)])
+        upper = np.array([255,int(15*255/100.0),int(60*255/100.0)])
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv,lower,upper)
+        # cv2.imshow("mask",mask)
+        # cv2.waitKey(1)
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+
+            # check for the right contour
+            r_max = 0
+            for c in contours:
+                _, radius = cv2.minEnclosingCircle(c)
+                if radius > r_max:
+                    r_max = radius
+                    cnt = c
+            epsilon = 0.04*cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,epsilon,True)
+            approx = approx.tolist()
+
+            self.corner_filtering(approx)
+
+            # This takes the corner_filter and reprojects the image
+            pts1 = np.float32([self.corners[0],self.corners[1],self.corners[2],self.corners[3]])
+            pts2 = np.float32([[0,300],[300,300],[300,0],[0,0]])
+            M = cv2.getPerspectiveTransform(pts1,pts2)
+            dst = cv2.warpPerspective(img,M,(300,300))
+            #cv2.imshow("Image source",img)
+
+            cv2.circle(img,(self.corners[0][0],self.corners[0][1]),5,[255,255,255],-1)
+            cv2.circle(img,(self.corners[1][0],self.corners[1][1]),5,[0,255,255],-1)
+            cv2.circle(img,(self.corners[2][0],self.corners[2][1]),5,[255,0,255],-1)
+            cv2.circle(img,(self.corners[3][0],self.corners[3][1]),5,[255,255,0],-1)
+            cv2.imshow("Source", img)
+            cv2.imshow("Projection",dst)
+            cv2.waitKey(1)
+
+            return dst
+
+
+    def corner_filtering(self, pts):
+
+        for i in range(len(self.corners)):
+            self.corner_filter[i][1:10] = self.corner_filter[i][0:9]
+            self.corner_filter[i][0] = pts[i][0]
+            if np.abs(pts[i][0][0] - self.corners[i][0]) > 10:
+                self.corners[i][0] = int(np.mean([item[0] for item in self.corner_filter[i]][0:10]))
+                self.corners[i][1] = int(np.mean([item[1] for item in self.corner_filter[i]][0:10]))
+                self.corner_filter[i][0] = self.corners[i]
+            else:
+                self.corners[i][0] = int(np.mean([item[0] for item in self.corner_filter[i]][0:5]))
+                self.corners[i][1] = int(np.mean([item[1] for item in self.corner_filter[i]][0:5]))
+
+
+
+
 
 if __name__ == '__main__':
 
     rospy.init_node('my_img_processor', anonymous=True)
 
     bt = ball_tracking()
-    
+
     try:
         rospy.spin()
     except KeyboardInterrupt:
